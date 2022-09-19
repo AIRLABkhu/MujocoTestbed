dde4414e61fe21eec380f4b864f1352cd1618e46
JeongWoonLee <75155964+TropicalPenguinn@users.noreply.github.com>
2022-09-19 10:47:54 +0900

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,6 +7,8 @@ ALGO=$2
 echo "Experiments started."
 for seed in $(seq 0 9)
 do
-    python mujoco_${ALGO}.py --task $TASK --epoch 200 --seed $seed --logdir $LOGDIR
+    python mujoco_${ALGO}.py --step-per-epoch 5000 --task $TASK --epoch 200 --seed $seed --logdir $LOGDIR
 done
 echo "Experiments ended."
+
+python ./tools --root-dir ./results/$TASK/$ALGO
